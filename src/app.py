9bc67105a09e3aa7e6611aba7c1192a5da5bba3d
ablue968 <ablue968@gmail.com>
2021-03-21 23:09:59 +0000

    done

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -14,6 +15,22 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+jackson_family.add_member({
+"first_name":"John",
+"lucky_numbers":[7, 13, 22],
+"age": 33
+})
+jackson_family.add_member({
+"first_name":"Jane",
+"lucky_numbers":[10, 14, 3],
+"age": 35
+})
+jackson_family.add_member({
+"first_name":"Jimmy",
+"lucky_numbers":[1],
+"age": 5
+})
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,13 +47,41 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+ 
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:index>', methods=['GET'])
+def get_person(index):
+
+    # this is how you can use the Family datastructure by calling its methods
+    person = jackson_family.get_member(index)
+    if person:
+        return jsonify(person), 200
+    return 'Not Found', 404
+    
+    
+
+@app.route('/member', methods=['POST'])
+def new_person():
+    cuerpo_peticion = request.data # texto plano
+    cuerpo_peticion_dict = json.loads(cuerpo_peticion) # diccionario python
+    jackson_family.add_member(cuerpo_peticion_dict)
+
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
+
+
+@app.route('/member/<int:index>', methods=['DELETE'])
+def delete_person(index):
+    member = jackson_family.delete_member(index)
+    # members = jackson_family.get_all_members()
+    if member:
+        body = {
+            "done": True
+        }
+        return jsonify(body), 200
+    return 'Not Found', 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -20,16 +19,25 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
+        member['last_name'] = self.last_name
         # fill this method and update the return
-        pass
+        if 'id' not in member:
+            member['id'] = self._generateId()
+
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        miembro = self.get_member(id)
+        if miembro: #"super pythonic" descriptivo, esto significa que if miembro is NOT None "tira pa' lante" y devuelve el diccionario llamado extrañamente "miembro"
+            self._members.remove(miembro)
+            return miembro
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for miembro in self._members:
+            if miembro['id'] == id:
+                return miembro   
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
